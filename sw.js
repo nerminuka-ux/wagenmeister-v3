1b713558c1006e5912fbead5cca9479b595e7e36
nerminuka-ux <nermin.uka@icloud.com>
2026-08-12 14:45:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='wagenmeister-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon180.png','./icon192.png','./icon512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+ }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
